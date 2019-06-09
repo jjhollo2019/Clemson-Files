26b579c6f66342e544e72679882c461031e1b637
jjhollo2019 <jeremy.j.holloway@gmail.com>
2019-06-09 16:13:36 -0400

    6/9/2019

--- Summer/3220/Projects/Project 3/hybrid.c
+++ Summer/3220/Projects/Project 3/hybrid.c
@@ -7,27 +7,31 @@
 #include <stdio.h>
 #include "hybrid.h"
 
-void mask(int index, int arena){
-  if(index == 1){
-    return;
-  }
-}
-
 char *bitmap_allocate(){
+  //declare a header pointer
   long long unsigned int *header_ptr;
   
+  //set the start pointer to the start of the arena
   char *start = arena_head[0];
+  //set the end to the last part of the memeory space
   char *end = start + ARENA_0_SIZE;
 
+  //iterate through the memory space while not at the end
+  //and while the memory pointer is used
   while(start != end && *start != 0) {
+    //move the size of one block
     start += ARENA_0_BLOCK_SIZE;
   }
 
+  //get the address of start
   header_ptr = (long long unsigned int *) start;
+  //point start to the header signature
   *header_ptr = HEADER_SIGNATURE;
 
+  //decrement the number of available blocks
   arena_count[0]--;
 
+  //return the pointer while leaving space for the header bits
   return start + 8;
 }
 
@@ -74,13 +78,6 @@ char *list_allocate( int arena ){
     return *block_ptr + 8;
   }
 }
-/* The release() function returns a valid block to the appropriate bitmap
- * or free list. The function performs several sanity checks on the memory
- * address passed by the caller, including alignment and a simple range
- * check. If the address passes those checks, a pointer to the header
- * (i.e., the start of the block) is constructed and the signature in
- * the header of checked. These sanity checks can use this code:
- */
 
 void release( char *release_ptr ) {
 
@@ -106,60 +103,6 @@ void release( char *release_ptr ) {
     return;
   }
 }
-
-/* The allocate() function finds a free block that matches the requested
- * size in number of bytes. (A byte is equal to the 8-bit char data type
- * in C, so char* pointers are used.)
- *
- * The function determines the arena with the appropriate blocksize that
- * will accommodate the requested size plus 8 extra bytes for the header.
- * The bitmap or free list of an arena is based on the starting address
- * of free blocks, but the allocate() function inserts a header in an
- * allocated block and returns a pointer that is just beyond the header.
- * Thus, if the free block that is selected starts at address 0x1000,
- * the function inserts the header at address 0x1000 and sends back the
- * address 0x1008 to the caller.
- *
- * The bitmap for arena 0 is indexed in a big-endian manner in each
- * 32-bit word in an array of bitmap words (i.e., from left-to-right).
- * So 0x8000000 in word 0 of the bitmap represents the first block in
- * arena 0, and 0x0001000 in word 2 of the bitmap represents block 79
- * (i.e., 2*32 + 15) in arena 0. A 0 in the bitmap indicates that the
- * block is free, and a 1 indicates that the block is allocated. Free
- * blocks in arena 0 are totally empty (i.e., they do not contain
- * pointers or status bits).
- *
- * For arena 0, the free block with the lowest address, if one is
- * present, should be the one allocated.
- *
- * The other two arenas each use the initial bytes of free blocks to
- * hold the next-block pointers to implement singly-linked free lists.
- * Thus free blocks in arenas 1 and 2 are not completely empty.
- *
- * For arenas 1 and 2, the free block at the head of the free list, if
- * one is present, should be the one allocated.
- *
- * Each allocated block has an 8-byte header that contains a 7-hex-digit
- * signature and a single-hex-digit index of the arena to which the
- * block belongs. The index will help the release() function identify
- * the appropriate arena. The header values are:
- * 
- * 0xab51ead0 => arena 0
- * 0xab51ead1 => arena 1
- * 0xab51ead2 => arena 2
- *
- * If the arena corresponding to the request size has no free blocks,
- * the allocate() function returns a NULL pointer. Also, if the function
- * is called with a size that is non-positive or that is too large to
- * fit within the largest blocksize, the function returns a NULL pointer.
- *
- * Note that, unlike some other popular memory allocation techniques,
- * if a particular arena is fully allocated, there is no splitting or
- * coalescing of block from other arenas and no system calls to obtain
- * more blocks.
- *
- * My allocate() function uses helper functions and looks like this:
- */
 
 char *allocate( int size ){
     if( size <= 0 ){
